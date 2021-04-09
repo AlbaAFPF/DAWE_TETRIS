abfb30ef49a401d920bf52c20d6656dbf9dad785
alba <albaarsuaga1@gmail.com>
2021-04-09 23:10:14 +0200

    Apartado 3.1 del ejercicio 2 hecho.

--- Práctica_Tetris_ejercicios/ejercicio2/tetris.js
+++ Práctica_Tetris_ejercicios/ejercicio2/tetris.js
@@ -4,9 +4,10 @@
 
 // ============== Point =======================
 
+// Representa la posición (x,y) de un punto en pantalla
 function Point (x, y) {
 	this.x = x;
-	this.y = y;    
+	this.y = y;
 }
 
 // ============== Rectangle ====================
@@ -25,10 +26,19 @@ Rectangle.prototype.draw = function() {
 
 	// TU CÓDIGO AQUÍ:
 	// pinta un rectángulo del color actual en pantalla en la posición px,py, con
-	// la anchura y altura actual y una línea de anchura=lineWidth. Ten en cuenta que 
-	// en este ejemplo la variable ctx es global y que guarda el contexto (context) 
-	// para pintar en el canvas.	
-	
+	// la anchura y altura actual y una línea de anchura=lineWidth. Ten en cuenta que
+	// en este ejemplo la variable ctx es global y que guarda el contexto (context)
+	// para pintar en el canvas.
+
+	// Rectángulo
+	ctx.fillStyle = this.color;
+	ctx.fillRect(this.px, this.py, this.width, this.height);
+	// Contorno del cuadrado
+	ctx.strokeStyle = "black";
+	ctx.lineWidth = this.lineWidth;
+	ctx.strokeRect(this.px, this.py, this.width, this.height);
+
+
 }
 
 
@@ -39,21 +49,39 @@ Rectangle.prototype.setFill = function(color) { this.color = color}
 
 function Block (pos, color) {
 
-	// TU CÓDIGO AQUÍ: este es el constructor de la clase Block. Recibe dos parámetros, pos y color. Pos = posición de la casilla, por ejemplo, (9,19).
+	// TU CÓDIGO AQUÍ: este es el constructor de la clase Block. Recibe dos parámetros,
+	// pos y color. Pos = posición de la celda, por ejemplo, (9,19).
 	// color = color que hay que emplear para pintar el bloque.
 	// Internamente este método crea dos puntos (empleando las coordenadas del pixel)
 	// y llama al método init de la clase Rectangle, pasándole como parámetro,
 	// estos dos puntos.
 	// Sería interesante que emplearas las constantes Block.BLOCK_SIZE y Block.OUTLINE_WIDTH,
-	// para establecer la anchura del bloque y la anchura de la línea, respectivamente.
+	// para establecer la anchura del bloque y la anchura de la línea.
+
+
+	// Cogemos las posiciones
+	this.x = pos.x;
+	this.y = pos.y;
+	// Y el color
+	this.color = color;
+
+	// Llamamos al método init de la clase Rectangle
+	var p1 = new Point((this.x*Block.BLOCK_SIZE), (this.y*Block.BLOCK_SIZE));
+	var p2 = new Point(((this.x+1)*Block.BLOCK_SIZE), ((this.y+1)*Block.BLOCK_SIZE));
+
+	this.init(p1,p2);
+	this.setLineWidth(Block.OUTLINE_WIDTH);
+	this.setFill(color);
+
 }
-
-
 
 Block.BLOCK_SIZE = 30;
 Block.OUTLINE_WIDTH = 2;
 
-// TU CÓDIGO AQUÍ: emplea el patrón de herencia (Block es un Rectangle)
+// TU CÓDIGO: emplea el patrón de herencia (Block es un Rectangle)
+
+// Block hereda de Rectangle
+Block.prototype = new Rectangle();
 
 
 // ************************************
@@ -71,12 +99,23 @@ Shape.prototype.init = function(coords, color) {
 	// Toma como parámetros: coords, un array de posiciones de los bloques
 	// que forman la Pieza y color, un string que indica el color de los bloques
 	// Post-condición: para cada coordenada, crea un bloque de ese color y lo guarda en un bloque-array.
+
+	this.blocks = [];
+
+	for(let i=0; i < coords.length; i++){
+		this.blocks.push(new Block(new Point(coords[i].x,coords[i].y), color));
+	}
+
 };
 
 Shape.prototype.draw = function() {
 
 	// TU CÓDIGO AQUÍ: método que debe pintar en pantalla todos los bloques
 	// que forman la Pieza
+	for(let i=0; i < this.blocks.length; i++){
+		this.blocks[i].draw();
+	}
+
 };
 
 // ============= I_Shape ================================
@@ -125,13 +164,13 @@ L_Shape.prototype = new Shape();
 function O_Shape(center) {
 
 	// TU CÓDIGO AQUÍ: Para programar O_Shape toma como ejemplo el código de la clase I_Shape
-	var coords = [new Point(center.x - 2, center.y),
+	var coords = [new Point(center.x - 2, center.y1),
 		new Point(center.x - 1, center.y),
 		new Point(center.x -1, center.y-1),
 		new Point(center.x-2, center.y-1)];
 
 }
-        
+
 // TU CÓDIGO AQUÍ: La clase O_Shape hereda de la clase Shape
 O_Shape.prototype = new Shape();
 
@@ -139,27 +178,40 @@ O_Shape.prototype = new Shape();
 function S_Shape(center) {
 
 	// TU CÓDIGO AQUÍ: Para programar S_Shape toma como ejemplo el código de la clase I_Shape 
-
+	var coords = [new Point(center.x - 2, center.y-1),
+		new Point(center.x - 1, center.y-1),
+		new Point(center.x -1, center.y),
+		new Point(center.x, center.y)];
 }
 
 // TU CÓDIGO AQUÍ: La clase S_Shape hereda de la clase Shape
+S_Shape.prototype = new Shape();
 
 // ============ T Shape ===========================
 function T_Shape(center) {
 
 	// TU CÓDIGO AQUÍ: : Para programar T_Shape toma como ejemplo el código de la clase I_Shape
-
+	var coords = [new Point(center.x - 2, center.y),
+		new Point(center.x - 1, center.y),
+		new Point(center.x, center.y),
+		new Point(center.x-1, center.y-1)];
 }
 
 // TU CÓDIGO AQUÍ: La clase T_Shape hereda de la clase Shape
+T_Shape.prototype = new Shape();
 
 
 // ============ Z Shape ===========================
 function Z_Shape(center) {
 
 	// TU CÓDIGO AQUÍ: : Para programar Z_Shape toma como ejemplo el código de la clase I_Shape
+	var coords = [new Point(center.x - 2, center.y),
+		new Point(center.x - 1, center.y),
+		new Point(center.x-1, center.y-1),
+		new Point(center.x, center.y-1)];
 }
 
 // TU CÓDIGO AQUÍ: La clase Z_Shape hereda de la clase Shape
+Z_Shape.prototype = new Shape();
 
 
